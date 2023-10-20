a7a59ebab37bad64b04f22d256d6635311456c0b
ErPiumino <118480890+ErPiumino@users.noreply.github.com>
2023-10-20 10:26:49 +0000

    Laboratorio 20 Ottobre

--- Lab3/PendoloSmorzatoFit.py
+++ Lab3/PendoloSmorzatoFit.py
@@ -6,18 +6,23 @@ from scipy.optimize import curve_fit
 x,y=np.loadtxt("fitSmorzato.dat",unpack=True)
 log_x = np.fabs(np.log(x))
 log_y = np.fabs(np.log(y))
-params = np.polyfit(log_x, log_y, 1)
-slope, intercept = params 
-plt.scatter(log_x, log_y, label='Dati', marker='x', color='Darkslategrey')
-plt.plot(log_x, slope*log_x+intercept, color='skyblue', label='fit scala logaritmica', alpha=0.8)
+#params = np.polyfit(log_x, log_y, 1)
+params = np.polyfit(x, y, 1)
+a, b = params
+#plt.scatter(log_x, log_y, label='Dati', marker='x', color='Darkslategrey')
+#plt.plot(log_x, a*log_x + b, color='skyblue', label='fit scala logaritmica', alpha=0.8)
+plt.scatter(x, y, label='Dati', marker='x', color='Darkslategrey')
+plt.plot(x, y, color='skyblue', label='fit lineare', alpha=0.8)
 
-plt.xscale('log')
-plt.yscale('log')
+#plt.xscale('log')
+#plt.yscale('log')
+
+m=np.fabs((np.log(y)-b)/np.log(x))
 
 plt.xlabel('$dt$')
 plt.ylabel('$e$')
-print('Il coff angolare:',slope)
-plt.text(5.5, 14, '$m=2.0184$', fontsize = 10)
+print('Il coff angolare:',m.mean())
+#plt.text(5.5, 14, '$m=2.0184$', fontsize = 10)
 plt.legend(loc='upper left')
 plt.savefig('FitPendoloSmorzato.pdf')
 plt.show()
--- Lab4/RK2.c
+++ Lab4/RK2.c
@@ -1,5 +1,4 @@
-//Pendolo usando Eulero-Cromer. Impossibile usare VerletVelocity perchè c'è la 
-//possibilità di inserire il coefficiente di attrito beta
+//Runge-Kutta using structs
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -9,57 +8,49 @@ struct Phase{
     double x;
     double v;
 };
+struct K{
+    double k1;
+    double k2;
+};
 
-void Pendolo(int argc, char **argv);
-struct Phase Passo(struct Phase xv, double omega2, double dt, double tmax, double b);
-double Energy(struct Phase xv, double omega2, double b);
-
+void Algoritmo(int argc, char **argv);
+struct Phase Passo(struct Phase xv, struct K k1, struct K k2, double omega2, double dt, double tmax);
+double Energy(struct Phase xv, double omega2);
 
 int main(int argc, char **argv){
-	Pendolo(argc, argv); 
+	Algoritmo(argc, argv); 
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void Pendolo (int argc, char **argv){
+void Algoritmo (int argc, char **argv){
     struct Phase xv;
-	double omega2, tmax, dt, b;
+    struct K k1;
+    struct K k2;
+	double omega2, tmax, dt;
 	double E0, E, Emax;
 	int k, i, n;
 	FILE *fp;
 	FILE *fit;
-	if(argc !=7){
-		printf("\nInput must be: theta0(deg) Omega2(rad/s) beta omega2(rad/s^2) tmax(s) dt(s). If you want to do the simulation of a simple pendulum without external force, just put b=0\n");
+	if(argc !=6){
+		printf("\nInput must be: theta0(deg) Omega2(rad/s) omega2(rad/s^2) tmax(s) dt(s).\n");
 		exit(1);
 	}
 	xv.x = atof(argv[1]);//atof legge un float, atoi per gli int
 	xv.v = atof(argv[2]);
-	b = atof(argv[3]);
-	omega2 = atof(argv[4]);
-	tmax = atof(argv[5]);
-	dt = atof(argv[6]);
-	char NomeFile1[20];
-	if(b==0){
-		sprintf(NomeFile1, "fit.dat", dt); //Salvo i dati dei vari dt in file diversi
-	}
-	else{
-		sprintf(NomeFile1, "fitSmorzato.dat", dt); //Salvo i dati dei vari dt in file diversi
-	}
-	fit=fopen(NomeFile1, "w");
+	omega2 = atof(argv[3]);
+	tmax = atof(argv[4]);
+	dt = atof(argv[5]);
+	fit=fopen("fit", "w");
 	for(k=0; k<=3; k++){ //Eseguo tre cicli perche' aumento il dt ogni volta
 		n=tmax/dt;
-		E0=Energy(xv, omega2, b);
-		char NomeFile[20];
-		if(b==0){
-			sprintf(NomeFile, "Pendolo%.3lf.dat", dt); //Salvo i dati dei vari dt in file diversi
-		}
-		else{
-			sprintf(NomeFile, "PendoloSmorzato%.3lf.dat", dt); //Salvo i dati dei vari dt in file diversi
-		}
+		E0=Energy(xv, omega2);
+		char NomeFile[25];
+		sprintf(NomeFile, "Algoritmo%.3lf.dat", dt); //Salvo i dati dei vari dt in file diversi
   		fp = fopen(NomeFile, "w");
 		fprintf(fp, "\t %.2lf \t %.2lf \t 0.00 \t %e\n", xv.x, xv.v, E0);
 		for(i=1; i<=n; i++){
-            xv = Passo(xv, omega2, dt, tmax, b);
-            E=Energy(xv, omega2, b);
+            xv = Passo(xv, k1, k2, omega2, dt, tmax);
+            E=Energy(xv, omega2);
             fprintf(fp,"\t %.2lf \t %.2lf \t %.2lf \t %e\n", xv.x, xv.v, i*dt, E);
 			if(Emax<E){
 				Emax=E; //Segno l'"Errore massimo" commesso in base al dt, altrimenti l'errore relativo sarebbe falsato
@@ -74,20 +65,18 @@ void Pendolo (int argc, char **argv){
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-double Energy(struct Phase xv, double omega2, double b){
+double Energy(struct Phase xv, double omega2){
     double E;
-    E=xv.v*xv.v + omega2*xv.x*xv.x -b*xv.v;
+    E=xv.v*xv.v + omega2*xv.x*xv.x;
     return E;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-struct Phase Passo(struct Phase xv, double omega2, double dt, double tmax, double b){
+struct Phase Passo(struct Phase xv, struct K k1, struct K k2, double omega2, double dt, double tmax){
     double x0, v0;
-    x0=xv.x;
-    v0=xv.v;
-	xv.v=v0 - omega2*x0*dt - b*v0;
-	xv.x=x0 + xv.v*dt;
-	x0=xv.x;
-	v0=xv.v;
+    x0 = xv.x;
+    v0 = xv.v;
+    k1.k1 = dt*xv.v;
+    k1.k2 = -omega2 * dt*xv.x;
     return xv;
 }
